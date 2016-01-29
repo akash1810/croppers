38ae1adb0a1c93fa7c4781051473780119f2a911
Akash A <akash1810@gmail.com>
2016-01-29 13:00:15 +0000

    initial commit

---
+++ app/scripts/main.js
@@ -0,0 +1,61 @@
+$(function () {
+  function getCell(table, cell) {
+    return document.getElementById(table).getElementsByClassName(cell)[0];
+  }
+
+  function getTable(tableId) {
+    const table = {};
+
+    ['x0', 'x1', 'y0', 'y1', 'width', 'height'].forEach(cell => table[cell] = getCell(tableId, cell));
+
+    return table;
+  }
+
+  function cropperjs () {
+    const Cropper = window.Cropper;
+    const image = document.getElementById('cropperjs-image');
+    const table = getTable('cropperjs-data');
+
+    new Cropper(image, {
+      aspectRatio: 1,
+      viewMode: 2,
+      crop: (data) => {
+        table['x0'].innerHTML = data.x;
+        table['x1'].innerHTML = data.width - data.x;
+        table['y0'].innerHTML = data.y;
+        table['y1'].innerHTML = data.height - data.y;
+        table['width'].innerHTML = data.width;
+        table['height'].innerHTML = data.height;
+      }
+    });
+  }
+
+  function jcrop () {
+    const image = document.getElementById('jcrop-image');
+    const initialCoords = [0, 0, image.naturalWidth, image.naturalHeight];
+    const table = getTable('jcrop-data');
+
+    const options = {
+      onChange: (data) => {
+        table['x0'].innerHTML = data.x;
+        table['x1'].innerHTML = data.x2;
+        table['y0'].innerHTML = data.y;
+        table['y1'].innerHTML = data.y2;
+        table['width'].innerHTML = data.w;
+        table['height'].innerHTML = data.h;
+      },
+      setSelect: initialCoords,
+      trueSize: [image.naturalWidth, image.naturalHeight]
+    };
+
+    $(image).Jcrop(options, function () {
+      this.setOptions({
+        aspectRatio: 1.0
+      })
+    });
+  }
+
+  cropperjs();
+  jcrop();
+});
+
